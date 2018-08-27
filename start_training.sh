45f873ba5d15e9a41c272375a9ff3060d1ab9487
manhtai <vo@manhtai.com>
2018-08-27 22:05:26 +0700

    Init repo

---
+++ start_training.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+python retrain.py \
+  --bottleneck_dir=tf_files/bottlenecks \
+  --how_many_training_steps=500 \
+  --model_dir=tf_files/models/ \
+  --summaries_dir=tf_files/training_summaries/"${ARCHITECTURE}" \
+  --output_graph=tf_files/retrained_graph.pb \
+  --output_labels=tf_files/retrained_labels.txt \
+  --architecture="${ARCHITECTURE}" \
+  --image_dir=/Volumes/manhtai.com/Zalo/TrainVal/
