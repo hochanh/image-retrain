8743ddc1c661316390b058bff02a56eae21b1a07
manhtai <vo@manhtai.com>
2018-09-01 09:38:16 +0700

    Change training params

--- start_labeling.sh
+++ start_labeling.sh
@@ -3,5 +3,7 @@ python label_image.py \
     --graph=tf_files/retrained_graph.pb \
     --labels=tf_files/retrained_labels.txt \
     --input_layer=Placeholder \
+    --input_height=224 \
+    --input_width=224 \
     --output_layer=final_result \
     --image_dir=$1
--- start_training.sh
+++ start_training.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 python retrain.py \
-  --bottleneck_dir=tf_files/bottlenecks \
-  --model_dir=tf_files/models/ \
-  --summaries_dir=tf_files/training_summaries/"${ARCHITECTURE}" \
+  --tfhub_module https://tfhub.dev/google/imagenet/mobilenet_v1_100_224/feature_vector/1 \
+  --how_many_training_steps=4000 \
+  --bottleneck_dir=/tmp/bottlenecks \
+  --summaries_dir=/tmp/training_summaries/mobilenet_v1_100_224 \
   --output_graph=tf_files/retrained_graph.pb \
   --output_labels=tf_files/retrained_labels.txt \
-  --architecture="${ARCHITECTURE}" \
-  --tfhub_module=https://tfhub.dev/google/imagenet/pnasnet_large/classification/2 \
+  --model_dir=tf_files/models/ \
   --image_dir=$1
