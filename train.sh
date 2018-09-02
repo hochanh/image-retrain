bcaf0f062106171aacd1ff3f8c2bdc15128436e3
manhtai <vo@manhtai.com>
2018-09-02 08:30:24 +0700

    Change to 10000 steps

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 python retrain.py \
   --tfhub_module https://tfhub.dev/google/imagenet/mobilenet_v1_100_224/feature_vector/1 \
-  --how_many_training_steps=5000 \
+  --how_many_training_steps=10000 \
   --bottleneck_dir=/tmp/bottlenecks \
   --summaries_dir=/tmp/training_summaries/mobilenet_v1_100_224 \
   --output_graph=tf_files/retrained_graph.pb \
